ecafedb510902713a9c540de10f9ccad653a5291
Artem Manchenkov <artyom@manchenkoff.me>
2019-10-07 22:10:40 +0300

    lesson 01 update

--- basic/first/03-functions.py
+++ basic/first/03-functions.py
@@ -6,19 +6,21 @@
 #  Работа с функциями, аргументы и возвращаемое значение
 #
 
-# def check_age(value):
-#     if value < 18:
-#         print("NOT OK")
-#     else:
-#         print("OK")
-#
-#
-# check_age(17)
-# check_age(18)
-# check_age(20)
-# check_age(10)
 
-# a = sum([1, 2, 3, 4, 5])
+def check_age(value):
+    if value < 18:
+        print("NOT OK")
+    else:
+        print("OK")
+
+
+check_age(17)
+check_age(18)
+check_age(20)
+check_age(10)
+
+a = sum([1, 2, 3, 4, 5])
+
 
 def say_hello(name="Guest"):
     return f"Hello, {name}"
