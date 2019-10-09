45e3d3d717214d93d0ecb8e65c0ee50ef12787ba
Artem Manchenkov <artyom@manchenkoff.me>
2019-10-09 20:59:46 +0300

    lesson 03 complete

--- basic/third/client.py
+++ basic/third/client.py
@@ -5,3 +5,22 @@
 #
 #  Пример клиента на Twisted
 #
+from twisted.internet import reactor
+from twisted.internet.protocol import ClientFactory
+from twisted.protocols.basic import LineOnlyReceiver
+
+
+class Client(LineOnlyReceiver):
+    def connectionMade(self):
+        self.sendLine("login:admin".encode())
+
+    def lineReceived(self, line):
+        print(line)
+
+
+class Connector(ClientFactory):
+    protocol = Client
+
+
+reactor.connectTCP("localhost", 7410, Connector())
+reactor.run()
--- basic/third/gui.py
+++ basic/third/gui.py
@@ -16,3 +16,31 @@
 #  3. pip install qt5reactor - установка пакета
 #  4. from PyQt5 import QtWidgets - подключить в файле .py
 #
+import sys
+from PyQt5 import QtWidgets
+from design import window
+
+
+class ExampleApp(QtWidgets.QMainWindow, window.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.submit_message)
+
+    def submit_message(self):
+        self.plainTextEdit.appendPlainText(
+            self.lineEdit.text()
+        )
+
+        self.lineEdit.clear()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
+
+
+main()
